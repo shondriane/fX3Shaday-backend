fbdb997b2200bdaf541797944a6f1b556a8a33bf
Shondriane <shondriane.wise@yahoo.com>
2022-12-08 09:10:07 -0500

    creating ReadMe file

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # FX3Shaday
+
+## Date 12/8/22
+
+### By:Shondriane Mesa-Wise
+
+### Shondriane Mesa-Wise: [Github](https://github.com/shondriane)|[LinkedIn](https://www.linkedin.com/in/shondriane-mesa-wise/
+
+
+***
+### ***Getting Started***
+1.  Fork and clone
+2.  npm install
+3.  npm run setup
+4.  npm run dev
+
+
+A trello board used to keep track of development progress can be [viewed here](https://trello.com/b/IzDMz1Dm/fx3).
+
+
+### ***Technologies Used***
+
+* PostgreSQL + Sequelize
+* Express js
+* React
+* Node js
+* JavaScript
+
+
+### ***Entity Hierarchy Diagram***
+![Entity Relationshiip Diagram](./assets/Screen%20Shot%202022-12-07%20at%205.26.51%20PM.png)
+
+### **_Future Features_**
+- set up payPal API
+
---
+++ migrations/20221207232537-create-user.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      userName: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      passwordDigest: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull:false,
+        unique:true,
+        validate:{
+          isEmail:true
+        }
+      },
+      phoneNumber: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
